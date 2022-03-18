ce9cf8ef7d130cb7d4de5da51a986bfdb60aba61
ravindrasinghshah <ravindrasinghshah@gmail.com>
2022-03-18 09:23:15 -0400

    error component

--- ClientApp/src/components/Error.js
+++ ClientApp/src/components/Error.js
@@ -1,9 +1,10 @@
 import { FaRegSadCry } from 'react-icons/fa';
+import { Link } from "react-router-dom";
 
 export default function Error() {
     return (
         <div className={style.wrapper}>
-            <FaRegSadCry className='mr-2' /> Oops! Something went wrong. Please try again later.
+            <FaRegSadCry className='mr-2' /> Oops! Something went wrong. Try searching from <Link to="/" className='underline pl-1'> home page</Link>.
         </div>
     )
 }
