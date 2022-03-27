832ccd39f100125ec6844eaecacf77a9a50ea8c5
ravindrasinghshah <ravindrasinghshah@gmail.com>
2022-03-27 12:45:37 -0400

    autocomplete new implementation

--- ClientApp/src/components/Autocomplete.js
+++ ClientApp/src/components/Autocomplete.js
@@ -1,152 +1,119 @@
-import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
+import { useEffect, useState } from 'react';
 
-class Autocomplete extends Component {
-    static propTypes = {
-        suggestions: PropTypes.instanceOf(Array)
-    };
+export default function Autocomplete(props) {
+    const books = props;
+    console.log(books)
+    // The active selection's index
+    const [activeSuggestion, setActiveSuggestion] = useState(0);
+    // The suggestions that match the user's input
+    const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+    // Whether or not the suggestion list is shown
+    const [showSuggestions, setShowSuggestions] = useState(false);
+    // What the user has entered
+    const [userInput, setUserInput] = useState('');
 
-    static defaultProps = {
-        suggestions: []
-    };
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            // The active selection's index
-            activeSuggestion: 0,
-            // The suggestions that match the user's input
-            filteredSuggestions: [],
-            // Whether or not the suggestion list is shown
-            showSuggestions: false,
-            // What the user has entered
-            userInput: ""
-        };
-    }
-
-    onChange = e => {
-        const { suggestions } = this.props;
+    const onChange = e => {
+        const { suggestions } = books;
         const userInput = e.currentTarget.value;
-        let filteredSuggestions = [];
-        // Filter our suggestions that don't contain the user's input
-        filteredSuggestions = suggestions.filter(
-            suggestion =>
-                suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
-        );
+         console.log(suggestions.history);
 
-        this.setState({
-            activeSuggestion: 0,
-            filteredSuggestions,
-            showSuggestions: true,
-            userInput: e.currentTarget.value
+        var filteredMatches = [];
+        suggestions.history.forEach(book => {
+            if (book.title.toLowerCase().indexOf(userInput.toLowerCase()) > -1)
+                filteredMatches.push(book.title);
         });
+
+        setActiveSuggestion(0);
+        setFilteredSuggestions(filteredMatches);
+        setUserInput(e.currentTarget.value);
+        setShowSuggestions(true);
     };
 
-    onClick = e => {
-        this.setState({
-            activeSuggestion: 0,
-            filteredSuggestions: [],
-            showSuggestions: false,
-            userInput: e.currentTarget.innerText
-        });
+    const onClick = e => {
+        setActiveSuggestion(0);
+        setFilteredSuggestions([]);
+        setUserInput(e.currentTarget.innerText);
+        setShowSuggestions(false);
     };
 
-    onKeyDown = e => {
-        const { activeSuggestion, filteredSuggestions } = this.state;
-
+    const onKeyDown = e => {
         // User pressed the enter key
         if (e.keyCode === 13) {
-            this.setState({
-                activeSuggestion: 0,
-                showSuggestions: false,
-                userInput: filteredSuggestions[activeSuggestion]
-            });
+            setActiveSuggestion(0);
+            setFilteredSuggestions([]);
+            setUserInput(filteredSuggestions[activeSuggestion]);
+            setShowSuggestions(false);
         }
         // User pressed the up arrow
         else if (e.keyCode === 38) {
             if (activeSuggestion === 0) {
                 return;
             }
-
-            this.setState({ activeSuggestion: activeSuggestion - 1 });
+            setActiveSuggestion(activeSuggestion - 1);
         }
         // User pressed the down arrow
         else if (e.keyCode === 40) {
             if (activeSuggestion - 1 === filteredSuggestions.length) {
                 return;
             }
-
-            this.setState({ activeSuggestion: activeSuggestion + 1 });
+            setActiveSuggestion(activeSuggestion + 1);
         }
     };
 
-    render() {
-        const {
-            onChange,
-            onClick,
-            onKeyDown,
-            state: {
-                activeSuggestion,
-                filteredSuggestions,
-                showSuggestions,
-                userInput
-            }
-        } = this;
 
-        let suggestionsListComponent;
+    let suggestionsListComponent;
 
-        if (showSuggestions && userInput.length > 1) {
-            if (filteredSuggestions.length) {
-                suggestionsListComponent = (
-                    <ul className="m-2">
-                        {filteredSuggestions.map((suggestion, index) => {
-                            let className = style.suggestions;
-                            // Flag the active suggestion with a class
-                            if (index === activeSuggestion) {
-                                className += style.activeSuggestion;
-                            }
+    if (showSuggestions && userInput.length > 1) {
+        if (filteredSuggestions.length) {
+            suggestionsListComponent = (
+                <ul className="m-2">
+                    {filteredSuggestions.map((suggestion, index) => {
+                        let className = style.suggestions;
+                        // Flag the active suggestion with a class
+                        if (index === activeSuggestion) {
+                            className += style.activeSuggestion;
+                        }
 
-                            return (
-                                <li className={className} key={index} value={suggestion} onClick={onClick}>
-                                    {suggestion}
-                                </li>
-                            );
-                        })}
-                    </ul>
-                );
-            } else {
-                suggestionsListComponent = (
-                    <div className={style.noSuggestions}>
-                        <em>No suggestions, you're on your own!</em>
-                    </div>
-                );
-            }
-        }
-
-        return (
-            <Fragment>
-                <div className={style.wrapper}>
-                    <input
-                        type="text"
-                        onChange={onChange}
-                        onKeyDown={onKeyDown}
-                        value={userInput}
-                        className={style.input}
-                        placeholder="For e.g. Education, Fiction etc."
-                    />
-                    {suggestionsListComponent}
+                        return (
+                            <li className={className} key={index} value={suggestion} onClick={onClick}>
+                                {suggestion}
+                            </li>
+                        );
+                    })}
+                </ul>
+            );
+        } else {
+            suggestionsListComponent = (
+                <div className={style.noSuggestions}>
+                    <em>No suggestions, you're on your own!</em>
                 </div>
-            </Fragment>
-        );
+            );
+        }
     }
+
+    return (
+        <>
+            <div className={style.wrapper}>
+                <input
+                    type="text"
+                    onChange={onChange}
+                    onKeyDown={onKeyDown}
+                    value={userInput}
+                    className={style.input}
+                    placeholder="For e.g. Education, Fiction etc."
+                />
+                {suggestionsListComponent}
+            </div>
+        </>
+    );
+
 }
 
-export default Autocomplete;
 
 const style = {
     wrapper: `bg-white border h-[200]`,
     input: `w-full rounded-sm h-10 py-1 px-2`,
     suggestions: `p-2 hover:bg-gray-200 cursor-pointer`,
-    activeSuggestion:` bg-gray-300`,
+    activeSuggestion: ` bg-gray-300`,
     noSuggestions: `mx-2 py-2`
 }
